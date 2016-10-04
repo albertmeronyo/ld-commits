ea8d4fb0320d4c150378794a7b477e351a54c3e7
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2016-10-04 09:50:49 +0200

    This is a multiline commit comment.
    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns\#\> .
    @prefix amp: <http://www.albertmeronyo.org/\> .
    
    amp:thiscommit rdf:type amp:commit .

--- ld-commits.py
+++ ld-commits.py
@@ -25,6 +25,7 @@ def post_hook():
     logger.debug('Received commit message: {}'.format(commit_message))
     # We'll ignore the first line of the commit message
     # Typically this line is by humans for humans
+
     
     logger.debug('Parsing triples from commit message')
     g = Graph()
