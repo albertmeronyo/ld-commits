ac7a1999035856da8173c3aab6db8e9f403f5c87
Albert Meronyo-Penyuela <albert.meronyo@gmail.com>
2016-11-29 13:16:32 +0100

    Return jsonld on get to main route
    <Albert <added> <jsonld> . <jsonld> <type> <foo> .

--- ld-commits.py
+++ ld-commits.py
@@ -2,9 +2,9 @@
 
 # ld-commits.py: backend for LD commits
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import json
-from rdflib import Graph
+from rdflib import Graph, URIRef, RDF, Namespace
 import logging
 
 app = Flask(__name__)
@@ -13,9 +13,28 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)-15s [%(levelname)s] (%(module)s.%(funcName)s) %(message)s')
 logger = logging.getLogger(__name__)
 
-@app.route('/')
+g = Graph()
+
+# endpoint_url = 'http://ld-commits.amp.ops.labs.vu.nl'
+
+# ldp_url = URIRef("http://www.w3.org/ns/ldp#")
+# ldp = Namespace(ldp_url)
+
+# g.add((URIRef(endpoint_url), RDF.type, ldp['Resource']))
+# g.add((URIRef(endpoint_url), RDF.type, ldp['RDFSource']))
+# g.add((URIRef(endpoint_url), RDF.type, ldp['Container']))
+# g.add((URIRef(endpoint_url), RDF.type, ldp['BasicContainer']))
+# g.bind('ldp', ldp)
+
+@app.route('/', methods=['GET'])
 def landing():
-    return 'LD-commits server here, listening to your POST commit HTTP requests :-)'
+    # return 'LD-commits server here, listening to your POST commit HTTP requests :-)'
+    resp = make_response(g.serialize(format='application/ld+json'))
+    resp.headers['X-Powered-By'] = 'https://github.com/albertmeronyo/ld-commits'
+    resp.headers['Allow'] = 'POST, GET'
+    resp.headers['Content-Type'] = 'application/ld+json'
+
+    return resp
 
 @app.route('/', methods=['POST'])
 def post_hook():
@@ -29,7 +48,7 @@ def post_hook():
     rdf_message = commit_message.split('\n', 1)[-1]
     
     logger.debug('Parsing triples from commit message')
-    g = Graph()
+
     g.parse(data=rdf_message, format='turtle')
     logger.debug('RDF graph parsed {} triples'.format(len(g)))
     
